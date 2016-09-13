7a10c7841af1ddb3de72f9a5b42fb89c62093d99
alexgibson <agibson@mozilla.com>
2016-09-13 09:41:02 +0100

    [fix bug 1299947] Firefox pre-release download pages

--- tests/redirects/map_external.py
+++ tests/redirects/map_external.py
@@ -9,5 +9,6 @@ from .base import flatten, url_test
 URLS = flatten((
     url_test('https://www.mozilla.com/', 'https://www.mozilla.org/firefox/'),
     url_test('https://aurora.mozilla.org/', 'https://www.mozilla.org/firefox/aurora/', status_code=requests.codes.found),
-    url_test('https://beta.mozilla.org/', 'https://www.mozilla.org/en-US/firefox/channel/#beta'),
+    # Bug 1299947 once new /channel pages are live we can update this redirect test.
+    # url_test('https://beta.mozilla.org/', 'https://www.mozilla.org/en-US/firefox/channel/#beta'),
 ))
